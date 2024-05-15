ff51ca4eae4807516f3ea8b1a6ccc197855c7b87
ankush170 <f20191095@goa.bits-pilani.ac.in>
2024-05-15 12:18:04 +0530

    routes for checking username and verification code added
    Signed-off-by: ankush170 <f20191095@goa.bits-pilani.ac.in>

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,7 +1,6 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User.model";
 import bcrypt from "bcryptjs";
-
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 
 export async function POST(request : Request){
